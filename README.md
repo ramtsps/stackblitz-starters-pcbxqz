9ab2d7f2b2171f5f47f6d4357f1b97bb15d09cb2
ramtsps <116824915+ramtsps@users.noreply.github.com>
2023-11-18 18:20:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-pcbxqz
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/ramtsps/stackblitz-starters-pcbxqz)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+import Home from "./hello/Home.js"
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <Home/>
+      <Home/>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
